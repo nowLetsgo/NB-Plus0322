e03fa9bc7bec7174b4be7d30c2f0c6ee312acc4b
nowLetsgo <lipeihua0922@163.com>
2021-06-10 18:34:59 +0800

    day08-ajax

--- day08-node08/04.ajax/server.js
+++ day08-node08/04.ajax/server.js
@@ -1,23 +1,42 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require("fs");
-const zlib = require("zlib");
 
+//body-parser中间件是处理post请求数据的
+const bodyParser = require("body-parser");
+//如果post请求发送的数据是json字符串格式，则使用下边处理
+app.use(bodyParser.json());
+//如果post请求发送的数据是form表单格式，则使用下边处理
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.get("/", (req, res) => {
     const filePath = path.resolve(__dirname, "./index.html");
     res.sendFile(filePath)
 })
 
-app.get("/login", (req, res) => {
+app.post("/login", (req, res) => {
+    const {
+        user,
+        pass
+    } = req.body;
+    console.log(req.body);
 
-
-    const data = {
-        mes: "ok",
-        code: 1
+    if (user === "laoli" && pass === "123") {
+        const data = {
+            mes: "ok",
+            code: 1
+        }
+        return res.json(data);
     }
-    res.json(data);
+
+
+    const err = {
+        mes: "no ok",
+        code: 0
+    }
+    return res.json(err);
 })
 
 app.listen("3000", (err) => {
