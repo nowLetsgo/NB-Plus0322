427cfaefd2c7284c078d71f5cb081a24a6f8af34
nowLetsgo <lipeihua0922@163.com>
2021-06-11 09:38:44 +0800

    day09-jqajax

---
+++ day09-ajax&ES6模块化/01.ajax/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//body-parser中间件是处理post请求数据的
+const bodyParser = require("body-parser");
+//如果post请求发送的数据是json字符串格式，则使用下边处理
+app.use(bodyParser.json());
+//如果post请求发送的数据是form表单格式，则使用下边处理
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get("/", (req, res) => {
+    const filePath = path.resolve(__dirname, "./index.html");
+    res.sendFile(filePath)
+})
+
+app.post("/login", (req, res) => {
+    const {
+        user,
+        pass
+    } = req.body;
+    console.log(req.body);
+
+    if (user === "laoli" && pass === "123") {
+        const data = {
+            mes: "ok",
+            code: 1
+        }
+        return res.json(data);
+    }
+
+
+    const err = {
+        mes: "no ok",
+        code: 0
+    }
+    return res.json(err);
+})
+
+app.get("/login", (req, res) => {
+    const {
+        user,
+        pass
+    } = req.query;
+    console.log(req.query);
+
+    if (user === "laoli" && pass === "123") {
+        const data = {
+            mes: "ok",
+            code: 1
+        }
+        return res.json(data);
+    }
+
+
+    const err = {
+        mes: "no ok",
+        code: 0
+    }
+    return res.json(err);
+})
+
+app.listen("3000", (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("http://127.0.0.1:3000");
+})
