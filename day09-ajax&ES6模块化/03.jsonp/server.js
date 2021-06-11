226c92321c17673811f35d02acd015d07d92a7c6
nowLetsgo <lipeihua0922@163.com>
2021-06-11 11:43:26 +0800

    day09-jsonp

---
+++ day09-ajax&ES6模块化/03.jsonp/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/login", (req, res) => {
+    const {
+        user,
+        pass,
+        cb
+    } = req.query;
+    console.log(req.query);
+
+    if (user === "laoli" && pass === "123") {
+        const data = {
+            mes: "ok",
+            code: 1
+        }
+        res.set("content-type", "application/javascript;charset=utf-8");
+        return res.send(`${cb}(${JSON.stringify(data)})`);
+
+    }
+
+
+    const err = {
+        mes: "no ok",
+        code: 0
+    }
+    return res.send(err);
+})
+
+app.listen("3000", (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("http://127.0.0.1:3000");
+})
